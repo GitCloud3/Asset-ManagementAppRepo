60cea32215868c9a2f7ed41205b3b73ec595dd60
GitCloud3 <sibonisor@gmail.com>
2015-06-14 10:52:17 +0200

    Added test case for createOwner

--- Asset-Management/src/test/java/za/co/authoritativelabpro/client/OwnerClientTest.java
+++ Asset-Management/src/test/java/za/co/authoritativelabpro/client/OwnerClientTest.java
@@ -4,6 +4,9 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.core.MediaType;
@@ -14,8 +17,8 @@ import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
 import org.jboss.resteasy.client.jaxrs.ResteasyWebTarget;
 import org.junit.Assert;
 import org.junit.Test;
-
 import za.co.authoritativelabpro.hmac.UrlSignerClient;
+import za.co.authoritativelabpro.model.Contact;
 import za.co.authoritativelabpro.model.Item;
 import za.co.authoritativelabpro.model.Owner;
 
@@ -25,7 +28,41 @@ public class OwnerClientTest {
 	static final String ROOT_URL = "http://localhost:8080/Asset-Management/declaration-ws/";
 
 	@Test
-	public void getOwners() throws IOException, InvalidKeyException, NoSuchAlgorithmException, URISyntaxException{
+	public void createOwner()  throws IOException, InvalidKeyException, NoSuchAlgorithmException, URISyntaxException {
+		
+		// Creation of Contacts hashmap to hold contacts
+		Set<Contact> contacts = new HashSet<Contact>();
+		
+		// Creation of contacts to added to hashmap of ontacts
+		Contact contact = new Contact(7, "8605169582089", "+277963254821", "lovemore@thuli.com");
+		Contact contact2 = new Contact(8, "8605169582089", "+27791527411", "lovemore@sbu.com");
+		contacts.add(contact);
+		contacts.add(contact2);
+		
+		// Creation of Items hashmap to hold items
+		Set<Item> items = new HashSet<Item>();
+		
+		// Creation of items to added to hashmap of Items
+		Item item = new Item("ERWRKWER145244", "8605169582089", "Computer", "Lenovo", "LN2552RT", "Lenovo Labs", "Black", "7708062158409", new Date());
+		items.add(item);
+		
+		// Adding owner data
+		Owner owner = new Owner("8605169582089", "Mr", "Lovemore", "Mabona", "Male", "African", "South Africa", "Gauteng", "MP", contacts, items);
+
+		ResteasyClient client = new ResteasyClientBuilder().build();
+		ResteasyWebTarget target = client.target(us.makeRESRCall(ROOT_URL + "createOwner", "unisa", "25125154dsad4da25="));
+		Response response = target.request().post(
+				Entity.entity(owner, MediaType.APPLICATION_JSON));
+
+		System.out.println("HTTP status" + response.getStatus());
+		// Read output in string format
+		String owners = response.readEntity(String.class);
+
+		System.out.println(owners);
+	}
+	
+	@Test
+	public void getOwners() throws IOException, InvalidKeyException, NoSuchAlgorithmException, URISyntaxException {
 		
 		ResteasyClient client = new ResteasyClientBuilder().build();
 		
