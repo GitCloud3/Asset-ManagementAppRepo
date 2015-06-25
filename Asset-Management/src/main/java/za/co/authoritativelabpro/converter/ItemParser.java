184aa1581c03a4a12796c1b5e67a5c73f3c11ab4
GitCloud3 <sibonisor@gmail.com>
2015-06-25 08:39:50 +0200

    Class that converts json list of items to java list of items

---
+++ Asset-Management/src/main/java/za/co/authoritativelabpro/converter/ItemParser.java
@@ -0,0 +1,36 @@
+package za.co.authoritativelabpro.converter;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.svenson.JSONParser;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import za.co.authoritativelabpro.model.Item;
+
+public class ItemParser {
+	
+	public List<Item> passser(final String json){
+		
+		List<Item> Items = new ArrayList<Item>();
+		
+		JSONParser parser = new JSONParser();
+		
+		parser.addTypeHint("Items[]", Item.class);
+		
+		@SuppressWarnings("unchecked")
+		Map<String, List<Item>> results1 = parser.parse(Map.class, json);
+		
+		for(Entry<String, List<Item>> entry : results1.entrySet()){
+			System.out.println("::: "+entry.getValue());
+			
+			TypeToken <List<Item>> tocken =  new TypeToken<List<Item>>(){};
+			Items = new Gson().fromJson(entry.getValue().toString(), tocken.getType());
+		}
+		return Items;
+	}
+}
