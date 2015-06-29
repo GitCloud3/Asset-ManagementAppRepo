d3a25a354ef58f9a84cd65609b362bb3af9a3f20
GitCloud3 <sibonisor@gmail.com>
2015-06-29 12:02:07 +0200

    adding a directive for delete confirmation

--- Asset-Management/src/main/webapp/AstMngrControllers.js
+++ Asset-Management/src/main/webapp/AstMngrControllers.js
@@ -57,6 +57,10 @@ app.controller('listCtrl',function($scope, $http)
 		$scope.myData.splice($scope.myData.indexOf(row),1);
 	}
 
+	$scope.editRecord = function(row){
+		alert("About to update");
+	}
+
 });
 
 app.controller('listOwnersCtrl',function($scope, $http, $routeParams)
@@ -127,4 +131,20 @@ app.controller('addItemCtrl',function($scope, $http, $location) {
 
 		$http.post('http://localhost:8080/Asset-Management/declaration-ws/createItem', data);
 	}
+});
+
+app.directive('confirmationNeeded', function(){
+	return{
+		priority: 1,
+		terminal: true,
+		link: function(scope, element, attr){
+			var msg = attr.confirmationNeeded || "Are you sure you want to delete?";
+			var clickAction = attr.ngClick;
+			element.bind('click', function(){
+				if(window.confirm(msg)){
+					scope.$eval(clickAction)
+				}
+			});
+		}
+	};
 });
