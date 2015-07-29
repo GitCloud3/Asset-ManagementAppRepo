5386a20355fb6e06eaf5db75a630f4df6e3e231a
GitCloud3 <sibonisor@gmail.com>
2015-07-29 19:44:01 +0200

    Adding a Generic class to cater for json list conversions of Items and Contacts to java list

---
+++ Asset-Management/src/main/java/za/co/authoritativelabpro/converter/TypeConverter.java
@@ -0,0 +1,41 @@
+package za.co.authoritativelabpro.converter;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.svenson.JSONParser;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+public class TypeConverter<T> 
+{
+	
+	final Class<T> typeParameterClass;
+	
+	public TypeConverter(Class<T> typeParameterClass)
+	{
+		this.typeParameterClass = typeParameterClass;
+	}
+	
+	public List<T> convert(final String json)
+	{
+		List<T> types = new ArrayList<T>();
+		
+		JSONParser parser = new JSONParser();
+		
+		parser.addTypeHint("Types[]", typeParameterClass);
+		
+		@SuppressWarnings("unchecked")
+		Map<String, List<T>> results1 = parser.parse(Map.class, json);
+		
+		for(Entry<String, List<T>> entry : results1.entrySet()){
+
+			TypeToken <List<T>> tocken =  new TypeToken<List<T>>(){};
+			types = new Gson().fromJson(entry.getValue().toString(), tocken.getType());
+		}
+		return types;
+	}
+}
